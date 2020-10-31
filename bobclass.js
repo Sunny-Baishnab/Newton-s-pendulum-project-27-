d1d7b7d5a66758c2734d981930b334019fafccf6
Sunny-Baishnab <72726626+Sunny-Baishnab@users.noreply.github.com>
2020-10-31 17:25:46 +0530

    Update bobclass.js

--- bobclass.js
+++ bobclass.js
@@ -2,17 +2,20 @@ class Bob {
     constructor(x,y){
         var options={
             isStatic:false,
-            density:1
+            density:1,
+            restitution:1
         }
-        this.body=Bodies.circle(x,y,20,options);
-        this.radius=20;
+        this.body=Bodies.circle(x,y,40,options);
+        this.radius=40;
         World.add(world,this.body);
     }
     display(){
+        var pos=this.body.position;
+        push();
+        translate(this.body.position.x,this.body.position.y);
         ellipseMode(RADIUS);
         fill("purple");
-        var pos=this.body.position;
-        ellipse(pos.x,pos.y,20,20);
-        
+        ellipse(pos.x,pos.y,40,40);
+        pop();
     }
 }
